29f28cc30f7ffad5febcda9e868396b29b6ee68b
Rushi Patel <prushir@udel.edu>
2021-03-16 16:24:59 -0400

    Added Button logic

--- music_website/src/index.js
+++ music_website/src/index.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Button } from "./Components/Button";
 
-
-var element = React.createElement('h1', { className: 'greeting' }, 'Hello World!');
-ReactDOM.render(element,
-  document.getElementById('root')
-);
+function App() {
+  return (
+    <div className="App">
+      <Button
+        onClick={()=> {
+          console.log("clicked")
+        }}
+        type = "button"
+        buttonStyle = "btn--primary--solid"
+        buttonSize = "btn--large"
+      >Click</Button>
+    </div>
+  );
+}
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
