0d802ad1bceab61745c50fb0402c594545060c4b
Rushi Patel <prushir@udel.edu>
2021-03-19 15:39:22 -0400

    Added button that plays mp3 file

--- music_website/src/Assignments/Assignment1.js
+++ music_website/src/Assignments/Assignment1.js
@@ -3,12 +3,35 @@ import { Button } from "./../Components/Button";
 import history from "./../history";
 import "./assignment.css";
 import { withRouter } from "react-router-dom";
+import {Howl, Howler} from 'howler';
+import Tmp from "./../tmp.mp3";
 
 class Assignment1 extends Component {
+    SoundPlay = (src) => {
+        const sound = new Howl({
+            src
+        })
+        sound.play();
+    }
+    RenderButtonAndSound = () => {
+            return(
+                <Button 
+                    onClick={() => 
+                    this.SoundPlay(Tmp)
+                    }
+                    type = "button"
+                    buttonStyle = "btn--primary--solid"
+                    buttonSize = "btn--large"
+                    >Play Sound
+                </Button>
+            )
+    }
   render() {
+      Howler.volume(1.0)
     return (
         <div className="Assignment1">
             <h2>Assignment 1</h2>
+        {this.RenderButtonAndSound()}
         <Button
           onClick={()=> {
             history.push('/');
