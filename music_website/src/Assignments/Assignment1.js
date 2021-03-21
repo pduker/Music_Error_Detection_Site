b4d43e86c15e78c732aa870478ab8411a803acb2
Yen-An Chen <andychen@udel.edu>
2021-03-21 18:35:49 -0400

    display music sheet and change  buttom margin

--- music_website/src/Assignments/Assignment1.js
+++ music_website/src/Assignments/Assignment1.js
@@ -3,54 +3,56 @@ import { Button } from "./../Components/Button";
 import history from "./../history";
 import "./assignment.css";
 import { withRouter } from "react-router-dom";
-import {Howl, Howler} from 'howler';
-import Tmp from "./../Audio/tmp.mp3";
+import { Howl, Howler } from 'howler';
+import Tmp from "./../music_example/Example1.m4a";
+import example1 from "./../music_example/example1.jpg";
 var count;
 
 class Assignment1 extends Component {
-    SoundPlay = (src) => {
-        const sound = new Howl({
-            src
-        })
-        sound.play();
-    }
-    RenderButtonAndSound = () => {
-            return(
-                <Button 
-                    onClick={() => {
-                        if (count < 3){
-                            this.SoundPlay(Tmp);
-                        }
-                        count++;
-                    }
-                    }
-                    type = "button"
-                    buttonStyle = "btn--primary--solid"
-                    buttonSize = "btn--large"
-                    >Play Sound
+  SoundPlay = (src) => {
+    const sound = new Howl({
+      src
+    })
+    sound.play();
+  }
+  RenderButtonAndSound = () => {
+    return (
+      <Button
+        onClick={() => {
+          if (count < 3) {
+            this.SoundPlay(Tmp);
+          }
+          count++;
+        }
+        }
+        type="button"
+        buttonStyle="btn--primary--solid"
+        buttonSize="btn--large"
+      >Play Sound: 
                 </Button>
-            )
-    }
+    )
+  }
   render() {
-      Howler.volume(1.0)
-    if (! count){
-        count = 0;
+    Howler.volume(1.0)
+    if (!count) {
+      count = 0;
     }
 
     return (
-        <div className="Assignment1">
-            <h2>Assignment 1</h2>
+      <div className="Assignment1">
+        <h2>Assignment 1</h2>
         {this.RenderButtonAndSound()}
         <Button
-          onClick={()=> {
+          onClick={() => {
             history.push('/');
             history.go();
           }
           }
-          type = "button"
-          buttonStyle = "btn--primary--solid"
-          buttonSize = "btn--large"
+          type="button"
+          buttonStyle="btn--primary--solid"
+          buttonSize="btn--large"
         >Go Back to Assignments</Button>
+        <img src={example1} alt="Assignment 1" />
       </div>
     );
   }
