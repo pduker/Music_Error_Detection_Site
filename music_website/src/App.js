ad487dccceea15be991fd06f4343716322f2c26b
mtdesimone <mtdes@udel.edu>
2021-05-02 13:59:29 -0400

    Bug fixed placement of hotspots and the bounds of where they can be placed

--- music_website/src/App.js
+++ music_website/src/App.js
@@ -10,33 +10,36 @@ function App() {
   const divStyleP = {
     display: 'none',
     position: 'absolute',
-    top: placeY-window.innerHeight*0.025,
-    left: placeX-window.innerWidth*0.015,
+    top: placeY - 19, //19 is 1/2 the size of the square hotspot
+    left: placeX - 19,
     background: 'Orchid',
     opacity: '.5',
     border: '2px solid BlueViolet',
     width: '38px',
-    height: '38px'};
+    height: '38px'}
+    ;
   const divStyleR = {
     display: 'none',
     position: 'absolute',
-    top: placeY-window.innerHeight*0.025,
-    left: placeX-window.innerWidth*0.015,
+    top: placeY-19,
+    left: placeX-19,
     background: 'Pink',
     opacity: '.5',
     border: '2px solid Red',
     width: '38px',
-    height: '38px'};
+    height: '38px'}
+    ;
   const divStyleI = {
     display: 'none',
     position: 'absolute',
-    top: placeY-window.innerHeight*0.025,
-    left: placeX-window.innerWidth*0.015,
+    top: placeY-19,
+    left: placeX-19,
     background: 'AquaMarine',
     opacity: '.5',
     border: '2px solid Green',
     width: '38px',
-    height: '38px'};
+    height: '38px'}
+    ;
   return (
     <div className="App">
       <Routes />
