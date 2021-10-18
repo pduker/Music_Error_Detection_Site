6f99a8bee6af3289021ab5c85eff3af7cfe69c11
Collin Reich <cdreich7@gmail.com>
2021-10-18 15:21:56 -0400

    sprint 2 commit

--- music_website/src/Components/AssignmentTemplate.js
+++ music_website/src/Components/AssignmentTemplate.js
@@ -13,8 +13,7 @@ class AssignmentTemplate extends Component {
     constructor(props){
         super(props);
         this.state = {
-            errorType: '',
-            count: 0,
+            currentErrorType: '',
             pitchErrorCount: 0,
             rythmErrorCount: 0,
             intonationErrorCount: 0,
@@ -37,32 +36,38 @@ class AssignmentTemplate extends Component {
             default:
                 console.log("UNKNOWN ERROR TYPE HAS BEEN SET");
         }
-        this.setState({ errorType: newErrorType });
+        this.setState({ currentErrorType: newErrorType });
     }
 
     render() { 
         return (
         <div className="assignment-template">
-            <h1 className="assignment-header">Assignment {/*insert assignment number here*/}</h1>
+            <h1 className="assignment-header">Assignment Template {this.props.assignmentNumber}</h1>
             <div className="instructions">
                 <p>{/*Styling should be done with CSS and not HTML tags*/}
                 Instruction: Click the "Play Sound" button to hear the music. 
                 You will only be able to play the sound 3 times. After listening to the music, 
                 place the hotspots over each note error. There are 3 differnt types of error:
                 Pitch Error (Purple), Rhythm Error (Red), and Intonation Error (Green).
+                <br></br>
                 How to place the hotspot?
+                <br></br>
                 1. Click the error button
+                <br></br>
                 2. Place the hotspot on the note error
+                <br></br>
                 3. Click the error button again.
+                <br></br>
                 4. If you want to add more errors, click on the note to place hotspot and click the error button.
+                <br></br>
                 5. After you are done, click the submit button to check your answer
                 </p>
             </div>
-            <Music url={audioExample1}></Music>
+            <Music url={this.props.audioUrl}></Music>
             <PitchErrorButton handler={this.handleErrorButtonClick}></PitchErrorButton>
             <RythmErrorButton handler={this.handleErrorButtonClick}></RythmErrorButton>
             <IntonationErrorButton handler={this.handleErrorButtonClick}></IntonationErrorButton>
-            <img id="img1" className="music-sheet" src={musicSheetExample1} alt="musicSheet"/>
+            <img id="img1" className="music-sheet" src={this.props.sheetMusic} alt="musicSheet"/>
             <button className="button back-button" onClick={() => {
                 history.push('/');
                 history.go();
--- music_website/src/Scripts/Routes.js
+++ music_website/src/Scripts/Routes.js
@@ -10,18 +10,24 @@ import Assignment5 from "../Assignments/Assignment5";
 import history from './history';
 import AssignmentTemplate from "../Components/AssignmentTemplate";
 
+//there has to be a better way than individually importing each file
+import audio1 from "../Resources/Audio/Example1.m4a";
+import sheetMusic1 from "../Resources/Images/example1.jpg";
+
 export default class Routes extends Component {
     render() {
+        // let assignment1 = <AssignmentTemplate/>
         return (
             <div>
             <Router history={history}>
                 <Switch>
                     <Route path="/" exact component={Home} />
-                    <Route path="/Assignment1" component={AssignmentTemplate} />
+                    <Route path="/Assignment1" component={Assignment1} />
                     <Route path="/Assignment2" component={Assignment2} />
                     <Route path="/Assignment3" component={Assignment3} />
                     <Route path="/Assignment4" component={Assignment4} />
                     <Route path="/Assignment5" component={Assignment5} />
+                    <Route path="/AssignmentTemplate" render={(props) => <AssignmentTemplate {...props} audioUrl={audio1} sheetMusic={sheetMusic1}/>} />
                 </Switch>
             </Router>
             </div>
